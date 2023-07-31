3db4aebc6db1a346f890de45b9447c816f8692b9
loac <admin@loac.cc>
2023-08-01 00:15:46 +0800

    First Commit.

--- src/main/kotlin/cc/loac/models/Customer.kt
+++
@@ -1,7 +0,0 @@
-package cc.loac.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
-val customerStorage = mutableListOf<Customer>()
