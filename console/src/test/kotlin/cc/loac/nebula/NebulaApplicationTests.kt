75769ec33e862a991c97ab6b27fa793a0f477399
Loac <admin@loac.cc>
2024-01-02 13:45:35 +0800

    update project

---
+++ console/src/test/kotlin/cc/loac/nebula/NebulaApplicationTests.kt
@@ -0,0 +1,13 @@
+package cc.loac.nebula
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NebulaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
