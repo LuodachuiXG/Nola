c4b0e7b2b4027a75b0748cba12bef9d149f7e600
loac <admin@loac.cc>
2025-05-08 18:16:23 +0800

    feat: 完成 Overview 响应类定义.

--- src/main/kotlin/cc/loac/data/responses/OverviewResponse.kt
+++ src/main/kotlin/cc/loac/data/responses/OverviewResponse.kt
@@ -5,15 +5,22 @@ import cc.loac.data.models.Post
 /**
  * 概述响应数据类
  * @param count 项目数量
+ * @param tags 标签列表
+ * @param categories 分类列表
  * @param mostViewedPost 浏览量最多的文章
+ * @param lastOperation 最近的一次操作
+ * @param lastLoginDate 最后登录时间
  * @param createDate 博客创建时间
  */
 data class OverviewResponse(
     val count: OverviewCount,
+    val tags: List<OverviewTag>,
+    val categories: List<OverviewCategory>,
     val mostViewedPost: Post,
+    val lastOperation: String,
+    val lastLoginDate: Long,
     val createDate: Long
 )
-
 
 /**
  * 概述项目数量
@@ -35,4 +42,25 @@ data class OverviewCount(
     val file: Int,
     val link: Int,
     val menu: Int
+)
+
+/**
+ * 概述标签数据
+ * @param tagId 标签 ID
+ * @param displayName 标签名
+ * @param postCount 文章数量
+ */
+data class OverviewTag(
+    val tagId: Long,
+    val displayName: String,
+    val postCount: Int
+)
+
+/**
+ * 概述分类数据
+ */
+data class OverviewCategory(
+    val categoryId: Long,
+    val displayName: String,
+    val postCount: Int
 )
