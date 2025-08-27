b5d1fb0bdbe64774358e896ddc90a4526b66aee0
loac <admin@loac.cc>
2025-08-27 17:11:16 +0800

    feat: ICP 添加部分兼容代码.

--- src/main/kotlin/cc/loac/data/models/ICPFiling.kt
+++ src/main/kotlin/cc/loac/data/models/ICPFiling.kt
@@ -4,8 +4,11 @@ package cc.loac.data.models
  * 工业信息化部备案信息数据类
  * @param icp ICP 备案号
  * @param police 公网安备号
+ * @param public 公网安备号（此字段已废弃，保留为兼容旧版，防止报错）
  */
 data class ICPFiling(
     val icp: String?,
-    val police: String?
+    val police: String?,
+    // 此字段已废弃，保留为兼容旧版，防止报错
+    val public: String?
 )
